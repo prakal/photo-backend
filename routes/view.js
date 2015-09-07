a45080490739f4a30d83942e2e427013c7293205
Prakal <pranav.89@gmail.com>
2015-09-07 17:51:39 -0600

    add logic for view/:number to display image based on url. need to increase view count.

--- routes/view.js
+++ routes/view.js
@@ -1,9 +1,19 @@
 // might need to escape url to prevent attacks
 var express = require('express');
 var router = express.Router();
+var db = require('../app/config.js')
 
-router.post('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:number', function(req, res, next) {
+	// provide image only, filtered by primary id in photos table, rendered as an image, not as json
+	// filter is the req.url.slice(1)
+	
+	db.knex('photos')
+		.where({'photos.id':req.url.slice(1)})
+		.select('image_url')
+		.then(function(data){
+			console.log(data[0]);
+  		res.end('<img src='+data[0].image_url+'>');
+		})
 });
 
 module.exports = router;
