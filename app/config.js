4ddcd7d90cd12257feaa3ac013c1bfbb5dc92be2
Prakal <pranav.89@gmail.com>
2015-09-07 16:40:55 -0600

    add config info for table photos

---
+++ app/config.js
@@ -0,0 +1,26 @@
+var knex =  !process.env.DATABASE_URL ? require('../local_config.js') :
+  require('knex')({
+  client: 'pg',
+  connection: process.env.DATABASE_URL
+});
+
+var db = require('bookshelf')(knex);
+db.plugin('registry');
+
+db.knex.schema.hasTable('photos').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('photos', function (table) {
+      //normal stuff
+      table.increments('id').primary();
+      table.string('image_url', 100).unique();//.notNullable();
+      table.integer('user_id');//.notNullable();
+      table.integer('group_id');//.notNullable();
+      table.integer('views');//.notNullable();
+      table.timestamps();
+    }).then(function (table) {
+      console.log('Created the table:', table);
+    });
+  }
+});
+
+module.exports = db;
