05177fe31e4b348b4b1702071b6474f8b94f9327
Prakal <pranav.89@gmail.com>
2015-09-07 15:56:33 -0600

    delete wrong server.js file

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ var express = require('express');
 var http 	= require('http');
 var path    = require('path');
 var app 	= express();
+var path    = require('path');
 
 // Routes
 var routes  = require('./../routes/index');
-app.use('/', routes);
 
+
+app.set('views', path.join(__dirname, '../client/www'));
+// add view engine as jade
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, '../client/www')));
+
+
+app.use('/', routes);
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
