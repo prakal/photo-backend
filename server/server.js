e9981ffe0124138694c0394a437466332731a687
Prakal <pranav.89@gmail.com>
2015-09-07 22:35:26 -0600

    modify server listen port for heroku

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, '../client/www')));
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
