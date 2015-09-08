753cc21be27793bbc7d3e22b26f70aa3980a46b4
Prakal <pranav.89@gmail.com>
2015-09-07 18:12:21 -0600

    add logic for incrementing view count

--- routes/view.js
+++ routes/view.js
@@ -9,10 +9,16 @@ router.get('/:number', function(req, res, next) {
 	
 	db.knex('photos')
 		.where({'photos.id':req.url.slice(1)})
-		.select('image_url')
-		.then(function(data){
-			console.log(data[0]);
-  		res.end('<img src='+data[0].image_url+'>');
+		.increment('views',1)
+		.then(function(){
+			db.knex('photos')
+				.where({'photos.id':req.url.slice(1)})	
+				.select('*')
+				.then(function(data){
+					console.log(data[0]);
+					// increase view count on photo based on primary key
+		  		res.end('<img src='+data[0].image_url+'>');
+				});
 		})
 });
 
