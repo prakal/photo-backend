07744d3bbadd36ae5da7696a918906efb011d8fd
Prakal <pranav.89@gmail.com>
2015-09-11 15:35:04 -0600

    uncomment console log to see if server has started

--- server/server.js
+++ server/server.js
@@ -30,14 +30,14 @@ app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, '../client/www')));
 
-app.server = app.listen(process.env.PORT || 3000, function () {
-  // var host = server.address().address;
-  // var port = server.address().port;
+var server = app.listen(process.env.PORT || 3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-  // console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%s', host, port);
 });
 
-var io 		= require('socket.io').listen(app.server);
+var io 		= require('socket.io').listen(server);
 // socket.io code
 io.on('connection', function(socket){
   console.log('a user connected');
